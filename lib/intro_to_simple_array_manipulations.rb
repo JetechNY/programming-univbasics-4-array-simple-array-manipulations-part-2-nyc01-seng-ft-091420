f75223784c1bf3d96c3a1a09a4c5689c4fba2abf
SunJet Liu <sunjetliu@gmail.com>
2020-09-01 21:53:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,24 +1,23 @@
 def using_concat(arr1, arr2)
   arr1.concat arr2
-
 end
 
-def using_insert
-
+def using_insert(arr1, ele)
+arr1.insert ele
 end
 
-def using_uniq
-
+def using_uniq(arr1)    
+arr1.uniq
 end
 
-def using_flatten
-
+def using_flatten(arr1)
+arr1.flatten
 end
 
-def using_delete()
-
+def using_delete(arr1, str)
+arr1.delete(str)
 end
 
-def using_delete_at
-
+def using_delete_at(arr1, int)
+arr1.delete_at(int)
 end
